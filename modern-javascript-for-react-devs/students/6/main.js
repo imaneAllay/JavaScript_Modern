f03f6c533d8280876c2e3fba3e70b557bcef21f4
Imane Allay <iallay@xpanxion.com>
2023-05-09 17:00:16 -0400

    Update exercice1

--- modern-javascript-for-react-devs/students/6/main.js
+++ modern-javascript-for-react-devs/students/6/main.js
@@ -6,6 +6,7 @@
 //_______________________________________________________
 
 //Exercice 0:
+//exercice 0 in modern-js
 
 const ex1 = () => {
     const array = [1, '2', 3, 'test', 1.2,"5"];
@@ -24,12 +25,19 @@ const ex3 = () => {
 // Your functions here...
 //
 
+//Exercice 1
 const countNumbers = (array) => {
-    let retval = 0;
-    // TODO...
-    return retval;
+    
+    
+    let c = 0;
+    for (let i=0;i<array.length;i++){
+        if (typeof(array[i])=='number'){
+            c++;
+        }
+        
+    }return c;
+    
 }
-
 const main = async () => {
     ex1();
 }
